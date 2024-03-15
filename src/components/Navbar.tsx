423bb91ed8abbe5212f14420239efadcabe43e70
foobar <uagnl25x@anonaddy.me>
2024-03-15 21:53:02 +0530

    refactored Navbar to diff component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => (
+    <nav className="absolute top-[90svh] ml-[50%] -translate-x-1/2 rounded-xl border bg-customBlack px-10 py-3">
+        <ul className="flex gap-6">
+            <li>Home</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
