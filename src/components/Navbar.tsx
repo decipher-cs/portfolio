7189296a114b83718274d2feb823eb1d3a55d736
foobar <uagnl25x@anonaddy.me>
2024-03-16 21:25:39 +0530

    fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 export const Navbar = () => (
-    <nav className="absolute top-[90svh] ml-[50%] -translate-x-1/2 rounded-xl border bg-customBlack px-10 py-3">
+    <nav className="fixed bottom-6 z-50 ml-[50%] -translate-x-1/2 rounded-xl border bg-customBlack px-10 py-3">
         <ul className="flex gap-6">
             <li>Home</li>
             <li>Projects</li>
