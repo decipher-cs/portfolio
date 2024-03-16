a9de0b224b070d011732e758f3c9cca1e09bef7a
foobar <uagnl25x@anonaddy.me>
2024-03-16 22:42:50 +0530

    updated configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
             colors: {
-                customBlack: '#4f4f4f',
-                customRed: '#ff2828',
+                customBlack: "#4f4f4f",
+                customRed: "#ff2828",
             },
             keyframes: {
                 horizontalScroll: {
-                    from: { transform: 'translateX(0)' },
-                    to: { transform: 'translateX(-100%)' },
+                    from: { transform: "translateX(0)" },
+                    to: { transform: "translateX(-50%)" },
                 },
             },
             animation: {
-                horizontalScroll: 'horizontalScroll linear 10s infinite',
+                horizontalScroll: "horizontalScroll linear 40s infinite",
             },
         },
     },
     plugins: [],
-};
+}
