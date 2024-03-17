1c521ea5b65eb40131ba13091796ed05e850b0ce
foobar <uagnl25x@anonaddy.me>
2024-03-17 22:42:47 +0530

    configured tailwindcss config to use different default fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme"
+import colors from "tailwindcss/colors"
 export default {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    content: ["./src/**/*.{js,ts,jsx,tsx}", "./index.html"],
     theme: {
         extend: {
+            fontFamily: {
+                sans: ["cantarell", ...defaultTheme.fontFamily.sans],
+                azonix: [
+                    "azonix",
+                    "'cantarell'",
+                    ...defaultTheme.fontFamily.sans,
+                ],
+            },
             colors: {
                 customBlack: "#4f4f4f",
+                customBlackDark: "#1e1e1e",
                 customRed: "#ff2828",
+                customBorder: colors.gray[700],
             },
             keyframes: {
                 horizontalScroll: {
