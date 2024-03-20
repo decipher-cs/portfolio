6dce74716752529083bc065738ae6d3c5918f27a
foobar <uagnl25x@anonaddy.me>
2024-03-20 20:28:57 +0530

    changed custom color names

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Button } from "./Button"
 
 export const Navbar = () => (
     <nav className="fixed bottom-6 z-50 ml-[50%] -translate-x-1/2 ">
-        <ul className="flex content-center items-center gap-6 rounded-xl border border-customBorder  bg-customBlackDark p-4">
+        <ul className="flex content-center items-center gap-6 rounded-xl border border-customBorder  bg-secondary p-4">
             <li>
                 <a href={"#" + "home"}>
                     <Button>Home</Button>
